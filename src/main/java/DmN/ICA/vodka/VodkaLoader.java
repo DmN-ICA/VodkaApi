32798fa070c535eac423d801e8ca0c04ad8a1bbb
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-29 18:13:44 +0700

    xD

--- src/main/java/DmN/ICA/vodka/VodkaLoader.java
+++ src/main/java/DmN/ICA/vodka/VodkaLoader.java
@@ -4,13 +4,21 @@ import DmN.ICA.vodka.api.MinecraftVersion;
 import DmN.ICA.vodka.api.VodkaMod;
 import DmN.ICA.vodka.impl.EnvType;
 import DmN.ICA.vodka.impl.LoaderType;
+import DmN.ICA.vodka.impl.VodkaClassLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Path;
+
 public abstract class VodkaLoader implements VodkaMod, VodkaMod.FirstInitializer, VodkaMod.CommonInitializer, VodkaMod.ClientInitializer, VodkaMod.ServerInitializer {
     public static final Logger LOGGER = LogManager.getLogger("Vodka[Loader/Api]");
     public static @NotNull VodkaLoader INSTANCE;
+    public final @NotNull VodkaClassLoader loader;
+
+    protected VodkaLoader(@NotNull VodkaClassLoader loader) {
+        this.loader = loader;
+    }
 
     @Override
     public void firstInit() {
@@ -49,4 +57,8 @@ public abstract class VodkaLoader implements VodkaMod, VodkaMod.FirstInitializer
     }
 
     public abstract @NotNull EnvType getEnvironment();
+
+    public abstract @NotNull Path getModsDir();
+
+    public abstract @NotNull Path getConfigDir();
 }
---
+++ src/main/java/DmN/ICA/vodka/impl/VodkaClassLoader.java
@@ -0,0 +1,20 @@
+package DmN.ICA.vodka.impl;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class VodkaClassLoader extends URLClassLoader {
+    public VodkaClassLoader(URL[] urls, ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    public VodkaClassLoader create(File modsDir, ClassLoader parent) throws MalformedURLException {
+        File[] files = modsDir.listFiles();
+        URL[] mods = new URL[files.length];
+        for (int i = 0; i < files.length; i++)
+            mods[i] = files[i].toURL();
+        return new VodkaClassLoader(mods, parent);
+    }
+}
