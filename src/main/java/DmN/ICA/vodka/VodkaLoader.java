8deff8df4e1c9ea24841db60c86cb86afb137369
DomamaN202 <51743763+Domaman202@users.noreply.github.com>
2022-05-29 17:10:01 +0700

    xD

--- src/main/java/DmN/ICA/vodka/VodkaLoader.java
+++ src/main/java/DmN/ICA/vodka/VodkaLoader.java
@@ -4,29 +4,32 @@ import DmN.ICA.vodka.api.MinecraftVersion;
 import DmN.ICA.vodka.api.VodkaMod;
 import DmN.ICA.vodka.impl.EnvType;
 import DmN.ICA.vodka.impl.LoaderType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class VodkaLoader implements VodkaMod, VodkaMod.FirstInitializer, VodkaMod.CommonInitializer, VodkaMod.ClientInitializer, VodkaMod.ServerInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("Vodka[Loader/Api]");
     public static @NotNull VodkaLoader INSTANCE;
 
     @Override
     public void firstInit() {
-
+        LOGGER.info("Called FirstInit");
     }
 
     @Override
     public void commonInit() {
-
+        LOGGER.info("Called CommonInit");
     }
 
     @Override
     public void clientInit() {
-
+        LOGGER.info("Called ClientInit");
     }
 
     @Override
     public void serverInit() {
-
+        LOGGER.info("Called ServerInit");
     }
 
     @Override
